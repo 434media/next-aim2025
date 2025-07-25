73372c5cb96d3d153ae08c2ba9346d9d33391464
Jesse Hernandez <jesseovr@gmail.com>
2025-07-24 21:04:15 -0500

    fixed compile error

--- components/hero/VideoPlayer.tsx
+++ components/hero/VideoPlayer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useRef, useState, useEffect, useCallback } from "react"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "motion/react"
 import { VisuallyHidden } from "../../components/ui/visually-hidden"
 import Image from "next/image"
 
