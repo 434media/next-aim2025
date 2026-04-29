cad11758013294832ca2ec21d8fc79172c78a82f
Jesse Hernandez <jesseovr@gmail.com>
2026-04-29 11:09:06 -0500

    cta text update

--- data/partners.ts
+++ data/partners.ts
@@ -2,28 +2,28 @@
  * Main partners and sponsors data
  */
 export const mainPartners = [
-    {
-      name: "COSA",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/cosa_quatrefoil_texas_k.png",
-      href: "https://www.sanantonio.gov/",
-    },
-    {
-      name: "VelocityTX",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/VelocityTX%20Logo%20MAIN%20RGB%20(1).png",
-      href: "https://velocitytx.org/",
-    },
-    {
-      name: "UT San Antonio",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/ut-san-antonio.svg",
-      href: "https://uthscsa.edu/",
-    },
-    {
-      name: "Bexar County Economic and Community Development",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/bexar-ecd.png",
-      href: "https://www.bexar.org/682/Economic-Community-Development",
-    },
-  ]
-  
+  {
+    name: "COSA",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/cosa_quatrefoil_texas_k.png",
+    href: "https://www.sanantonio.gov/",
+  },
+  {
+    name: "VelocityTX",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/VelocityTX%20Logo%20MAIN%20RGB%20(1).png",
+    href: "https://velocitytx.org/",
+  },
+  {
+    name: "UT San Antonio",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/ut-san-antonio.svg",
+    href: "https://uthscsa.edu/",
+  },
+  {
+    name: "Bexar County Economic and Community Development",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/bexar-ecd.png",
+    href: "https://www.bexar.org/682/Economic-Community-Development",
+  },
+]
+
 /**
  * AIM 2026 Sponsors — Tier 1
  */
@@ -143,45 +143,44 @@ export const aim2026SponsorsBottom = [
   },
 ]
 
-  /**
-   * Additional partners and sponsors data
-   */
-  export const additionalPartners = [
-    {
-      name: "DHA",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/dha_logo.png",
-      href: "https://www.health.mil/",
-    },
-    {
-      name: "USAISR",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/USAISR_LOGO_HI_RES%20blank%20background%20black%20and%20white.jpg",
-      href: "https://usaisr.amedd.army.mil/",
-    },
-    {
-      name: "NAMRU",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/namru.png",
-      href: "https://www.med.navy.mil/Naval-Medical-Research-Center/",
-    },
-    {
-      name: "59th",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/59th_Medical_Wing.png",
-      href: "https://www.59mdw.af.mil/",
-    },
-    {
-      name: "VA",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Logotype-on-Light.svg",
-      href: "https://www.va.gov/",
-    },
-    {
-      name: "BAMC",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/bamc.png",
-      href: "https://bamc.tricare.mil/",
-    },
-    {
-      name: "711th Human Performance Wing",
-      src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/711%20(1).png",
-      href: "https://www.afrl.af.mil/711HPW/",
-    },
-  ]
-  
-  
+/**
+ * Additional partners and sponsors data
+ */
+export const additionalPartners = [
+  {
+    name: "DHA",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/dha_logo.png",
+    href: "https://www.health.mil/",
+  },
+  {
+    name: "USAISR",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/USAISR_LOGO_HI_RES%20blank%20background%20black%20and%20white.jpg",
+    href: "https://usaisr.amedd.army.mil/",
+  },
+  {
+    name: "NAMRU",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/namru.png",
+    href: "https://www.med.navy.mil/Naval-Medical-Research-Center/",
+  },
+  {
+    name: "59th",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/59th_Medical_Wing.png",
+    href: "https://www.59mdw.af.mil/",
+  },
+  {
+    name: "VA",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Logotype-on-Light.svg",
+    href: "https://www.va.gov/",
+  },
+  {
+    name: "BAMC",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/bamc.png",
+    href: "https://bamc.tricare.mil/",
+  },
+  {
+    name: "711th Human Performance Wing",
+    src: "https://storage.googleapis.com/groovy-ego-462522-v2.firebasestorage.app/Sponsor%20Logos/711%20(1).png",
+    href: "https://www.afrl.af.mil/711HPW/",
+  },
+]
+
